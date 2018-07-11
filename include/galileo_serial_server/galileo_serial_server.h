d4f4c882aee5cd82066f37f1c0b964da222234c6
xiaoqiang <xiaoqiang@bwbot.org>
2018-07-11 19:41:55 +0800

    format code and add charge status

--- include/galileo_serial_server/galileo_serial_server.h
+++ include/galileo_serial_server/galileo_serial_server.h
@@ -1,5 +1,32 @@
-#ifndef galileoSERIALSERVER_H
-#define galileoSERIALSERVER_H
+/******************************************************************************
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Bluewhale Robot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Author: Xiefusheng, Randoms
+ *******************************************************************************/
+
+#ifndef __GALILEO_SERIAL_SERVER_H__
+#define __GALILEO_SERIAL_SERVER_H__
 
 #include "galileo_serial_server/AsyncSerial.h"
 
@@ -11,31 +38,37 @@
 
 namespace galileo_serial_server
 {
-typedef struct {
-  int nav_status;//导航服务状态,0 表示没开启 closed,1 表示开启 opened 。
-  int visual_status;//视觉系统状态,0 表示没初始化 uninit,1 表示正在追踪 tracking,2表示丢失 lost,1 和 2 都表示视觉系统已经初始化完成。
-  float power;//电源电压【9 46】v 。
-  int target_numID;//当前目标点编号,默认值为-1 表示无效值。
-  int target_status;//当前目标点状态,0 表示已经到达或者取消 free,1 表示正在前往目标点过程中 working,2 表示当前目标点的移动任务被暂停 paused,3 表示目标点出现错误 error, 默认值为-1 表示无效值。
-  float target_distance;//机器人距离当前目标点的距离,单位为米,-1 表示无效值,该值的绝对值小于 0.01 时表示已经到达。
-  int angle_goal_status;//目标角度达到情况,0 表示未完成,1 表示完成,2 表示 error,默认值为-1 表示无效值。
-  float control_speed_x;//导航系统计算给出的前进速度控制分量,单位为 m/s 。
-  float control_speed_theta;//导航系统计算给出的角速度控制分量,单位为 rad/s 。
-  float current_speed_x;//当前机器人实际前进速度分量,单位为 m/s 。
-  float current_speed_theta;//当前机器人实际角速度分量,单位为 rad/s 。
-  unsigned int time_stamp;//时间戳,单位为 1/30 毫秒,用于统计丢包率。
-}DOWNLOAD_STATUS;
+typedef struct
+{
+    int nav_status;  //导航服务状态,0 表示没开启 closed,1 表示开启 opened 。
+    int visual_status;  //视觉系统状态,0 表示没初始化 uninit,1 表示正在追踪 tracking,2表示丢失 lost,1 和 2
+                        //都表示视觉系统已经初始化完成。
+    int charge_status;  //充电状态，0 free 未充电状态, 1 charging 充电中, 2 charged 已充满，但仍在小电流充电, 3 finding
+                        //寻找充电桩, 4 docking 停靠充电桩, 5 error 错误
+    float power;       //电源电压【9 46】v 。
+    int target_numID;  //当前目标点编号,默认值为-1 表示无效值。
+    int target_status;  //当前目标点状态,0 表示已经到达或者取消 free,1 表示正在前往目标点过程中 working,2
+                        //表示当前目标点的移动任务被暂停 paused,3 表示目标点出现错误 error, 默认值为-1 表示无效值。
+    float target_distance;  //机器人距离当前目标点的距离,单位为米,-1 表示无效值,该值的绝对值小于 0.01 时表示已经到达。
+    int angle_goal_status;  //目标角度达到情况,0 表示未完成,1 表示完成,2 表示 error,默认值为-1 表示无效值。
+    float control_speed_x;      //导航系统计算给出的前进速度控制分量,单位为 m/s 。
+    float control_speed_theta;  //导航系统计算给出的角速度控制分量,单位为 rad/s 。
+    float current_speed_x;      //当前机器人实际前进速度分量,单位为 m/s 。
+    float current_speed_theta;  //当前机器人实际角速度分量,单位为 rad/s 。
+    unsigned int time_stamp;    //时间戳,单位为 1/30 毫秒,用于统计丢包率。
+} DOWNLOAD_STATUS;
 
 class StatusPublisher
 {
-public:
-    StatusPublisher(std::string galileoCmds_topic,std::string galileoStatus_topic,CallbackAsyncSerial* cmd_serial);
+  public:
+    StatusPublisher(std::string galileoCmds_topic, std::string galileoStatus_topic, CallbackAsyncSerial* cmd_serial);
     void Refresh();
-    void UpdateCmds(const char *data, unsigned int len);
-    void UpdateStatus(const galileo_serial_server::GalileoStatus & current_receive_status);
+    void UpdateCmds(const char* data, unsigned int len);
+    void UpdateStatus(const galileo_serial_server::GalileoStatus& current_receive_status);
     void run();
     DOWNLOAD_STATUS car_status;
-private:
+
+  private:
     ros::NodeHandle mNH_;
     ros::Publisher mgalileoCmdsPub_;
     bool mbUpdated_;
@@ -44,10 +77,9 @@ private:
     CallbackAsyncSerial* cmd_serial_;
     boost::mutex mStausMutex_;
     boost::mutex mCmdsMutex_;
-    char * cmd_str_;
+    char* cmd_str_;
 };
 
-} //galileo_serial_server
+}  // namespace galileo_serial_server
 
-
-#endif // galileoSERIALSERVER_H
+#endif
--- src/galileo_serial_server_node.cpp
+++ src/galileo_serial_server_node.cpp
@@ -1,3 +1,30 @@
+/******************************************************************************
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Bluewhale Robot
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Author: Xiefusheng, Randoms
+ *******************************************************************************/
+
 #include "galileo_serial_server/AsyncSerial.h"
 #include <iostream>
 #include <boost/thread.hpp>
@@ -8,9 +35,9 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    cout<<"welcome to galileo serial server,please feel free at home!"<<endl;
+    ROS_INFO_STREAM("welcome to galileo serial server,please feel free at home!");
 
     ros::init(argc, argv, "galileo_serial_server");
     ros::start();
@@ -20,29 +47,31 @@ int main(int argc, char **argv)
     ros::param::param<std::string>("~port", port, "/dev/ttyUSB1");
     int baud;
     ros::param::param<int>("~baud", baud, 115200);
-    cout<<"port:"<<port<<" baud:"<<baud<<endl;
+    ROS_INFO_STREAM("port:" << port << " baud:" << baud);
 
-    string galileoCmds_topic,galileoStatus_topic;
+    string galileoCmds_topic, galileoStatus_topic;
     ros::param::param<std::string>("~galileoCmds_topic", galileoCmds_topic, "/galileoSerialServer/cmds");
     ros::param::param<std::string>("~galileoStatus_topic", galileoStatus_topic, "/galileoSerialServer/status");
 
-    try {
-        CallbackAsyncSerial serial(port,baud);
-        galileo_serial_server::StatusPublisher galileo_server(galileoCmds_topic,galileoStatus_topic,&serial);
-        serial.setCallback(boost::bind(&galileo_serial_server::StatusPublisher::UpdateCmds,&galileo_server,_1,_2));
-        boost::thread cmd2serialThread(&galileo_serial_server::StatusPublisher::run,&galileo_server);
+    try
+    {
+        CallbackAsyncSerial serial(port, baud);
+        galileo_serial_server::StatusPublisher galileo_server(galileoCmds_topic, galileoStatus_topic, &serial);
+        serial.setCallback(boost::bind(&galileo_serial_server::StatusPublisher::UpdateCmds, &galileo_server, _1, _2));
+        boost::thread cmd2serialThread(&galileo_serial_server::StatusPublisher::run, &galileo_server);
 
-        ros::Rate r(30);//发布周期为50hz
+        ros::Rate r(30);  //发布周期为50hz
         while (ros::ok())
         {
-            galileo_server.Refresh();//定时发布状态
+            galileo_server.Refresh();  //定时发布状态
             r.sleep();
         }
-        quit:
+    quit:
         serial.close();
-
-    } catch (std::exception& e) {
-        cerr<<"Exception: "<<e.what()<<endl;
+    }
+    catch (std::exception& e)
+    {
+        ROS_ERROR_STREAM("Exception: " << e.what());
     }
     ros::shutdown();
     return 0;
