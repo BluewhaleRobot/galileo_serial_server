2e67092cbca34a177fa73aec307873638d30a7b9
xiaoqiang <xiaoqiang@bwbot.org>
2020-08-25 13:44:26 +0800

    完善下传状态

--- include/galileo_serial_server/galileo_serial_server.h
+++ include/galileo_serial_server/galileo_serial_server.h
@@ -40,22 +40,28 @@ namespace galileo_serial_server
 {
 typedef struct
 {
-    int nav_status;  //导航服务状态,0 表示没开启 closed,1 表示开启 opened 。
-    int visual_status;  //视觉系统状态,0 表示没初始化 uninit,1 表示正在追踪 tracking,2表示丢失 lost,1 和 2
-                        //都表示视觉系统已经初始化完成。
-    int charge_status;  //充电状态，0 free 未充电状态, 1 charging 充电中, 2 charged 已充满，但仍在小电流充电, 3 finding
-                        //寻找充电桩, 4 docking 停靠充电桩, 5 error 错误
-    float power;       //电源电压【9 46】v 。
-    int target_numID;  //当前目标点编号,默认值为-1 表示无效值。
-    int target_status;  //当前目标点状态,0 表示已经到达或者取消 free,1 表示正在前往目标点过程中 working,2
-                        //表示当前目标点的移动任务被暂停 paused,3 表示目标点出现错误 error, 默认值为-1 表示无效值。
-    float target_distance;  //机器人距离当前目标点的距离,单位为米,-1 表示无效值,该值的绝对值小于 0.01 时表示已经到达。
-    int angle_goal_status;  //目标角度达到情况,0 表示未完成,1 表示完成,2 表示 error,默认值为-1 表示无效值。
-    float control_speed_x;      //导航系统计算给出的前进速度控制分量,单位为 m/s 。
-    float control_speed_theta;  //导航系统计算给出的角速度控制分量,单位为 rad/s 。
-    float current_speed_x;      //当前机器人实际前进速度分量,单位为 m/s 。
-    float current_speed_theta;  //当前机器人实际角速度分量,单位为 rad/s 。
-    unsigned int time_stamp;    //时间戳,单位为 1/30 毫秒,用于统计丢包率。
+  int nav_status;// 导航服务状态，0表示没开启closed，1表示开启opened。
+  int visual_status;// 视觉系统状态，-1标系视觉系统处于关闭状态，0表示没初始化uninit，1表示正在追踪tracking,2表示丢失lost,1和2都表示视觉系统已经初始化完成。
+  int map_status; // 建图服务状态，0表示未开始建图，1表示正在建图
+  int gc_status; // 内存回收标志，0表示未进行内存回收，1表示正在进行内存回收
+  int gba_status; // 闭环优化标志，0表示未进行闭环优化，1表示正在进行闭环优化
+  int charge_status; // 充电状态，0 free 未充电状态, 1 charging 充电中, 2 charged 已充满，但仍在小电流充电, 3 finding
+                     // 寻找充电桩, 4 docking 停靠充电桩, 5 error 错误
+  int loop_status; // 是否处于自动巡检状态，1为处于，0为不处于。
+  float power;// 电源电压【946】v。
+  int target_numID;// 当前目标点编号,默认值为-1表示无效值，当正在执行无ID的任务是值为-2，比如通过Http API 创建的导航任务。
+  int target_status;// 当前目标点状态，0表示已经到达或者取消free，1表示正在前往目标点过程中working,2表示当前目标点的移动任务被暂停paused,3表示目标点出现错误error,默认值为-1表示无效值。
+  float target_distance;// 机器人距离当前目标点的距离，单位为米，-1表示无效值，该值的绝对值小于0.01时表示已经到达。
+  int angle_goal_status;// 目标角度达到情况，0表示未完成，1表示完成，2表示error,默认值为-1表示无效值。
+  float control_speed_x;// 导航系统计算给出的前进速度控制分量,单位为m/s。
+  float control_speed_theta;// 导航系统计算给出的角速度控制分量,单位为rad/s。
+  float current_speed_x;// 当前机器人实际前进速度分量,单位为m/s。
+  float current_speed_theta;// 当前机器人实际角速度分量,单位为rad/s。
+  unsigned int time_stamp;// 时间戳,单位为1/30毫秒，用于统计丢包率。对于ROS API时间戳在状态的header里面
+  float current_pose_x; // 当前机器人在map坐标系下的X坐标,此坐标可以直接用于设置动态插入点坐标
+  float current_pose_y; // 当前机器人在map坐标系下的Y坐标
+  float current_angle; // 当前机器人在map坐标系下的z轴转角(yaw)
+  int busy_status; //当busy为true时系统将仍然后接收新指令，但是不会立即处理。当系统退出busy状态后再处理消息
 } DOWNLOAD_STATUS;
 
 class StatusPublisher
--- src/galileo_serial_server.cpp
+++ src/galileo_serial_server.cpp
@@ -10,25 +10,36 @@ StatusPublisher::StatusPublisher(std::string galileoCmds_topic, std::string gali
 {
     mbUpdated_ = false;
     mgalileoCmdsPub_ = mNH_.advertise<galileo_serial_server::GalileoNativeCmds>(galileoCmds_topic_, 1, true);
-    car_status.nav_status = 0;
-    car_status.visual_status = 0;
-    car_status.charge_status = 0;
-    car_status.power = 0.0f;
-    car_status.target_numID = -1;
-    car_status.target_status = -1;
-    car_status.target_distance = -1.0f;
-    car_status.angle_goal_status = -1;
-    car_status.control_speed_x = 0.0f;
-    car_status.control_speed_theta = 0.0f;
-    car_status.current_speed_x = 0.0f;
-    car_status.current_speed_theta = 0.0f;
-    car_status.time_stamp = 0;
-    cmd_str_ = new char[53];
+
+    car_status.nav_status = 0;// 导航服务状态，0表示没开启closed，1表示开启opened。
+    car_status.visual_status = 0;// 视觉系统状态，-1标系视觉系统处于关闭状态，0表示没初始化uninit，1表示正在追踪tracking,2表示丢失lost,1和2都表示视觉系统已经初始化完成。
+    car_status.map_status = 0; // 建图服务状态，0表示未开始建图，1表示正在建图
+    car_status.gc_status = 0; // 内存回收标志，0表示未进行内存回收，1表示正在进行内存回收
+    car_status.gba_status = 0; // 闭环优化标志，0表示未进行闭环优化，1表示正在进行闭环优化
+    car_status.charge_status = 0; // 充电状态，0 free 未充电状态, 1 charging 充电中, 2 charged 已充满，但仍在小电流充电, 3 finding
+                     // 寻找充电桩, 4 docking 停靠充电桩, 5 error 错误
+    car_status.loop_status = 0; // 是否处于自动巡检状态，1为处于，0为不处于。
+    car_status.power = 0.f;// 电源电压【946】v。
+    car_status.target_numID = -1;// 当前目标点编号,默认值为-1表示无效值，当正在执行无ID的任务是值为-2，比如通过Http API 创建的导航任务。
+    car_status.target_status = -1;// 当前目标点状态，0表示已经到达或者取消free，1表示正在前往目标点过程中working,2表示当前目标点的移动任务被暂停paused,3表示目标点出现错误error,默认值为-1表示无效值。
+    car_status.target_distance = -1.f;// 机器人距离当前目标点的距离，单位为米，-1表示无效值，该值的绝对值小于0.01时表示已经到达。
+    car_status.angle_goal_status = -1;// 目标角度达到情况，0表示未完成，1表示完成，2表示error,默认值为-1表示无效值。
+    car_status.control_speed_x = 0.f;// 导航系统计算给出的前进速度控制分量,单位为m/s。
+    car_status.control_speed_theta = 0.f;// 导航系统计算给出的角速度控制分量,单位为rad/s。
+    car_status.current_speed_x = 0.f;// 当前机器人实际前进速度分量,单位为m/s。
+    car_status.current_speed_theta = 0.f;// 当前机器人实际角速度分量,单位为rad/s。
+    car_status.time_stamp = 0;// 时间戳,单位为1/30毫秒，用于统计丢包率。对于ROS API时间戳在状态的header里面
+    car_status.current_pose_x = 0.f; // 当前机器人在map坐标系下的X坐标,此坐标可以直接用于设置动态插入点坐标
+    car_status.current_pose_y = 0.f; // 当前机器人在map坐标系下的Y坐标
+    car_status.current_angle = 0.f; // 当前机器人在map坐标系下的z轴转角(yaw)
+    car_status.busy_status = 0; //当busy为true时系统将仍然后接收新指令，但是不会立即处理。当系统退出busy状态后再处理消息
+
+    cmd_str_ = new char[89];
     cmd_str_[0] = (char)0xcd;
     cmd_str_[1] = (char)0xeb;
     cmd_str_[2] = (char)0xd7;
-    cmd_str_[3] = (char)0x31;
-    cmd_str_[52] = (char)0x00;
+    cmd_str_[3] = (char)0x55;
+    cmd_str_[88] = (char)0x00;
 }
 
 void StatusPublisher::Refresh()
@@ -36,7 +47,7 @@ void StatusPublisher::Refresh()
     boost::mutex::scoped_lock lock(mStausMutex_);
     car_status.time_stamp += 1;
     int* receive_byte = (int*)&car_status;
-    memcpy(&cmd_str_[4], &receive_byte[0], 48);
+    memcpy(&cmd_str_[4], &receive_byte[0], 84);
     if (NULL != cmd_serial_)
     {
         cmd_serial_->write(cmd_str_, 53);
@@ -123,9 +134,14 @@ void StatusPublisher::UpdateCmds(const char* data, unsigned int len)
 void StatusPublisher::UpdateStatus(const galileo_serial_server::GalileoStatus& current_receive_status)
 {
     boost::mutex::scoped_lock lock(mStausMutex_);
+
     car_status.nav_status = current_receive_status.navStatus;
     car_status.visual_status = current_receive_status.visualStatus;
+    car_status.map_status = current_receive_status.mapStatus;
+    car_status.gc_status = current_receive_status.gcStatus;
+    car_status.gba_status = current_receive_status.gbaStatus;
     car_status.charge_status = current_receive_status.chargeStatus;
+    car_status.loop_status = current_receive_status.loopStatus;
     car_status.power = current_receive_status.power;
     car_status.target_numID = current_receive_status.targetNumID;
     car_status.target_status = current_receive_status.targetStatus;
@@ -135,6 +151,10 @@ void StatusPublisher::UpdateStatus(const galileo_serial_server::GalileoStatus& c
     car_status.control_speed_theta = current_receive_status.controlSpeedTheta;
     car_status.current_speed_x = current_receive_status.currentSpeedX;
     car_status.current_speed_theta = current_receive_status.currentSpeedTheta;
+    car_status.current_pose_x = current_receive_status.currentPosX;
+    car_status.current_pose_y = current_receive_status.currentPosY;
+    car_status.current_angle = current_receive_status.currentAngle;
+    car_status.busy_status = current_receive_status.busyStatus;
 }
 
 void StatusPublisher::run()
